52b9e861d672022b6c3f5d7fc33c05fdf9988c9c
Henoker <hennybany@gmail.com>
2023-07-11 22:19:43 +0300

    chore: config django logging

--- medium_api/settings/base.py
+++ medium_api/settings/base.py
@@ -49,7 +49,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "core_apps.profiles",
     "core_apps.common",
-    "core_apps.user",
+    "core_apps.users",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
@@ -89,16 +89,16 @@ WSGI_APPLICATION = "medium_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
-    }
-}
-
 # DATABASES = {
-#     "default": env.db("DATABASE_URL")
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": "mydatabase",
+#     }
 # }
+
+DATABASES = {
+    "default": env.db("DATABASE_URL")
+}
 
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
@@ -156,3 +156,24 @@ MEDIA_ROOT = str(ROOT_DIR / "mediafiles")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_URL_REGEX = r"^api/.*$"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {"console": {
+        "level": "DEBUG", 
+        "class":"logging.StreamHandler",
+        "formatter": "verbose",
+        }    
+    },
+    "root":{
+        "level": "INFO",
+        "handlers": ["console"]
+    },
+}
